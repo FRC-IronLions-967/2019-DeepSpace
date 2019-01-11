8f7d134efdd01e3b6939aef5e128e57ac65948ba
nastark123 <43659552+nastark123@users.noreply.github.com>
2019-01-10 20:10:55 -0600

    Added splitarcade class

---
+++ src/main/java/frc/lib/util/SplitArcadeDrive.java
@@ -0,0 +1,49 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.lib.util;
+
+/**
+ * Add your docs here.
+ */
+public class SplitArcadeDrive {
+    double xAxis;
+    double yAxis;
+    double R = yAxis - xAxis;
+    double L = yAxis + xAxis;
+    double max;
+    public SplitArcadeDrive(double xAxis, double yAxis) {
+        this.xAxis = xAxis;
+        this.yAxis = yAxis;
+        findMax();
+    }
+
+    public void findMax() {
+        max = Math.abs(L);
+        if(Math.abs(R) > max) {
+            max = Math.abs(R);
+        }
+    }
+
+    public double getR() {
+        if((Math.abs(yAxis) <= 1) && (Math.abs(xAxis) <= 1) && (max < 1)) {
+            return R;
+        } else {
+            return (R/max);
+        }
+    }
+    
+
+    public double getL() {
+        if((Math.abs(yAxis) <= 1) && (Math.abs(xAxis) <= 1) && (max < 1)) {
+            return L;
+        } else {
+            return (L/max);
+        }
+    }
+    
+}
