cb244550846c5732b1a7ff35652bc7ec4193a72e
Matt Behnke <matthewbehnke0@gmail.com>
2019-01-23 22:29:41 -0600

    Created Navagation Subsystm

---
+++ src/main/java/frc/robot/subsystems/NavagationSubsystem.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * This is the place for all navagation code to go 
+ */
+public class NavagationSubsystem extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  public NavagationSubsystem() {
+
+  }
+
+  public void disabledinit() {
+
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
