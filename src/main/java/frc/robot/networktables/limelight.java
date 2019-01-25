740e527658c1a1460433c4d80d1c8c0b6dcb8fe3
Matt Behnke <matthewbehnke0@gmail.com>
2019-01-24 19:22:38 -0600

    created networktables folder

--- src/main/java/frc/robot/networktables/limelight.java
+++ src/main/java/frc/robot/networktables/limelight.java
@@ -1,11 +1,5 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+package frc.robot.networktables;
 
-package frc.lib.util;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
@@ -14,12 +8,13 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 /**
  * Add your docs here.
  */
-public class TestVision {
+public class limelight {
+
     private NetworkTable table;
     private NetworkTableEntry tx;
     private NetworkTableEntry ty;
     private NetworkTableEntry ta;
-    public TestVision() {
+    public limelight() {
         table = NetworkTableInstance.getDefault().getTable("limelight");
         tx = table.getEntry("tx");
         ty = table.getEntry("ty");
