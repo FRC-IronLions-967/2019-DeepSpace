0b40b5b9544a73c0d82fee39f20d7c0152c27191
nastark123 <43659552+nastark123@users.noreply.github.com>
2019-01-26 09:33:02 -0600

    Added ability to get acceleration values from navx mxp

---
+++ src/main/java/frc/robot/subsystems/NavigationSubsystem.java
@@ -0,0 +1,57 @@
+package frc.robot.subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * This is the place for all navigation code to go 
+ */
+public class NavigationSubsystem extends Subsystem {
+  public  AHRS gyro;
+
+  public NavigationSubsystem() {
+
+    try {
+      gyro = new AHRS(SPI.Port.kMXP);
+		} catch(RuntimeException ex) {
+			DriverStation.reportError("Error instantiating navX MXP: " + ex.getMessage(), true);
+		}
+
+		gyro.zeroYaw();
+
+  }
+
+  public boolean resetYaw() {
+	  gyro.zeroYaw();
+	  return true;
+  }
+  
+  public double getYaw() {
+	  return gyro.getYaw();
+  }
+
+  public double getAccelerationX() {
+    return gyro.getRawAccelX();
+  }
+
+  public double getAccelerationY() {
+    return gyro.getRawAccelY();
+  }
+
+  public double getAccelerationZ() {
+    return gyro.getRawAccelZ();
+  }
+
+  public void disabledinit() {
+
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
