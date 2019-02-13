06be0d2abe610fea203c5574dcf73231f701192c
Matt Behnke <matthewbehnke0@gmail.com>
2019-02-12 21:17:24 -0600

    Created splitarcade lookup command

---
+++ src/main/java/frc/robot/commands/drivesubsystem/SplitArcadeLookUpCommand.java
@@ -0,0 +1,41 @@
+
+package frc.robot.commands.drivesubsystem;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class SplitArcadeLookUpCommand extends Command {
+  public SplitArcadeLookUpCommand() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires(Robot.m_driveSubsystem);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    Robot.m_driveSubsystem.arcadeDriveLookup(Robot.m_oi.xbox0.getRawAxis(1), -Robot.m_oi.xbox0.getRawAxis(4));
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
