99ccb334e0f791511a42e8daa2992cd7b7f8b600
nastark123 <43659552+nastark123@users.noreply.github.com>
2019-03-05 19:47:36 -0600

    Added base wrapper class for TalonSRX and VictorSPX and changed motor controllers in drive subsystem to thie wrapper classes

---
+++ src/main/java/frc/lib/util/IronVictorSPX.java
@@ -0,0 +1,20 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.lib.util;
+
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+/**
+ * Add your docs here.
+ */
+public class IronVictorSPX extends VictorSPX{
+    public IronVictorSPX(int port) {
+        super(port);
+        super.setInverted(false);
+    }
+}
